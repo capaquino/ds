d5758711376cf52e7029c957d3a7f34a9724c447
Nathan <nad46@zips.uakron.edu>
2018-03-30 16:37:29 -0400

    Added quadratic hashing. Needs to be investigated because of no noticeable improvement over linear probing. Not sure if that is expected or not...

--- proj3/316p3.cpp
+++ proj3/316p3.cpp
@@ -4,6 +4,7 @@
 
 #include "316p3.h"
 
+/* LinearHashTable methods */
 LinearHashTable::LinearHashTable(int s) : tableSize(s), collisions(0) {}
 
 bool LinearHashTable::hash(int key)
@@ -15,10 +16,10 @@ bool LinearHashTable::hash(int key)
     }
     else
     {
-        collisions += 1; // collisions count for each linear probe too?
         int start = hash;
         while (willCollide(hash))
         {
+            collisions += 1; // collisions count for each linear probe too?
             // move collisions here... collisions += 1;
             hash += 1;
             if (hash > tableSize) // loop to beginning
@@ -47,10 +48,62 @@ bool LinearHashTable::willCollide(int hash)
     return false;
 }
 
+/* QuadraticHashTable methods */
+QuadraticHashTable::QuadraticHashTable(int s) : tableSize(s), collisions(0) {}
 
+bool QuadraticHashTable::hash(int key)
+{
+    int hash = key % tableSize;
+    if (!willCollide(hash))
+    {
+        hashList.push_back(hash);
+    }
+    else
+    {
+        int start = hash;
+        int base = 2;
+        int power = 0;
+        while (willCollide(hash))
+        {
+            collisions += 1; // collisions count for each linear probe too?
+            hash = start;
+            hash += base ^ power;
+            power += 1;
+            // should be if or while... depends on failure condition
+            while (hash > tableSize) // going over table size...
+            {
+                hash -= tableSize;
+            }
+            /*
+            if (hash == start) // quadratic now is greater than start...
+            {
+                return false; // whats the failure condition for quadratic???
+            }
+            */
+        }
+        hashList.push_back(hash);
+    }
+    return true;
+}
+
+bool QuadraticHashTable::willCollide(int hash)
+{
+    for (int h : hashList)
+    {
+        if (h == hash)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+/* Program */
 int main(int argc, char const *argv[])
 {
     srand(time(NULL));
+    // might need to generate ints and put to a file to be used by all hashing
+    // functions so the results are comparable...
 
     /* Output table formatting */
     const int firstColWidth = 14;
@@ -61,9 +114,10 @@ int main(int argc, char const *argv[])
         std::cout << std::left << std::setw(numColWidth) << input_size;
     }
     std::cout << std::endl;
-    std::cout << std::left << std::setw(firstColWidth) << "Linear: ";
 
     /* Linear Probing */
+    std::cout << std::left << std::setw(firstColWidth) << "Linear: ";
+
     for (int input_size = 1000; input_size <= 9000; input_size += 1000)
     {
         LinearHashTable table = LinearHashTable(10000);
@@ -76,15 +130,27 @@ int main(int argc, char const *argv[])
                 break;
             }
         }
-
-/**/    std::cout << std::left << std::setw(numColWidth) << table.getCollisions();
+        std::cout << std::left << std::setw(numColWidth) << table.getCollisions();
     }
-
     std::cout << std::endl;
 
     /* Quadratic Hashing */
     std::cout << std::left << std::setw(firstColWidth) << "Quadratic: ";
 
+    for (int input_size = 1000; input_size <= 9000; input_size += 1000)
+    {
+        QuadraticHashTable table = QuadraticHashTable(10000);
+
+        for (int i = 0; i < input_size; i++)
+        {
+            if (!table.hash(rand() % INT_MAX))
+            {
+                std::cout << "Table full!" << std::endl;
+                break;
+            }
+        }
+        std::cout << std::left << std::setw(numColWidth) << table.getCollisions();
+    }
     std::cout << std::endl;
 
     /* Double Hashing */
--- proj3/316p3.h
+++ proj3/316p3.h
@@ -12,5 +12,20 @@ public:
     bool hash(int key);
     bool willCollide(int hash);
     int getCollisions() { return collisions; }
-    int getHashesUsed() { return hashList.size(); }
+    int getHashesUsed() { return hashList.size(); } // debug
+};
+
+class QuadraticHashTable
+{
+private:
+    int tableSize;
+    int collisions;
+    std::vector<int> hashList;
+
+public:
+    QuadraticHashTable(int s);
+    bool hash(int key);
+    bool willCollide(int hash);
+    int getCollisions() { return collisions; }
+    int getHashesUsed() { return hashList.size(); } // debug
 };
